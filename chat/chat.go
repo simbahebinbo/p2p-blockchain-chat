d9f3abff27fb10b13b6ec1ecd4860db0faceab21
Elias Tracy <elitracy19@gmail.com>
2024-09-01 11:11:30 -0500

    added chat features and new modules

--- chat/chat.go
+++ chat/chat.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"blocks"
 	"encoding/hex"
 	"fmt"
+	"github.com/elitracy/chat-blockchain/blocks"
+	"log"
+	"time"
 )
 
 func main() {
@@ -12,18 +14,18 @@ func main() {
 		print(err.Error())
 	}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1; i++ {
 		blockchain.AddBlock(fmt.Sprintf("Block: %d", i))
 	}
 
-	for i := 0; i < len(blockchain.Blocks); i++ {
-		fmt.Println(blockchain.Blocks[i].Data)
-		fmt.Printf(" - parent  > %s\n", hex.EncodeToString(blockchain.Blocks[i].ParentHash[:]))
-		fmt.Printf(" - current > %s\n", hex.EncodeToString(blockchain.Blocks[i].Hash[:]))
-		fmt.Println("-------------------------------------")
-	}
+	// for i := 0; i < len(blockchain.Blocks); i++ {
+	// 	fmt.Println(blockchain.Blocks[i].Data)
+	// 	fmt.Printf(" - parent  > %s\n", hex.EncodeToString(blockchain.Blocks[i].ParentHash[:]))
+	// 	fmt.Printf(" - current > %s\n", hex.EncodeToString(blockchain.Blocks[i].Hash[:]))
+	// 	fmt.Println("-------------------------------------")
+	// }
 
-	_, err = blockchain.CheckBlocks()
+	err = blockchain.CheckBlocks()
 
 	if err != nil {
 		print(err.Error())
@@ -31,4 +33,25 @@ func main() {
 	}
 
 	println("VALID BLOCKCHAIN")
+
+	for i := 0; i < len(blockchain.Blocks); i++ {
+		encodedBlock := blocks.SerializeBlock(blockchain.Blocks[i])
+		decodedBlock := blocks.DeserializeBlock(encodedBlock)
+
+		var unmarshaledTime time.Time
+
+		err := unmarshaledTime.UnmarshalBinary(decodedBlock.Timestamp)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		timeString := unmarshaledTime.Format(time.RFC3339)
+
+		fmt.Printf(" - timestamp > %s\n", timeString)
+		fmt.Printf(" - Data      > %s\n", decodedBlock.Data)
+		fmt.Printf(" - parent    > %s\n", hex.EncodeToString(decodedBlock.ParentHash[:]))
+		fmt.Printf(" - current   > %s\n", hex.EncodeToString(decodedBlock.Hash[:]))
+		fmt.Println("-------------------------------------")
+	}
+
 }
